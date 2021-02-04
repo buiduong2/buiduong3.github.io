f0d19cccfcc703489e29ded8475bd8f42654c916
buiduong2 <buiducduong1@gmail.com>
2021-02-04 20:30:02 +0700

    Chữa bài tập buổi-2

--- BT-buoi-2/BT2/src/Fibonacci.java
+++ BT-buoi-2/BT2/src/Fibonacci.java
@@ -6,26 +6,33 @@ public class Fibonacci {
          * Bài 2: 2 Viết chương trình cho phép nhập vào một số nguyên dương n, liệt kê n
          * số Fibonacci đầu tiên.
          */
+        // đọc hiểu sai yêu cầu đề bài. Đề yêu cầu liệt kê n số chứ ko phải các số sau
+        // chữ n
+        // thay vì dùng if có thể cộng thẳng số sau vào số trước như thế thì luôn đúng
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập vào 1 số nguyên Dương bất kỳ");
         int n = sc.nextInt();
         int number = 1;
         int previousNumber = 0;
+        int nextNumber;
         if (n < 0) {
             System.out.println("Giá trị nhập vào không chính xác");
-        } else if (n >0) {
-            System.out.println("Dãy số cần tìm là: ");
-            System.out.print(previousNumber + ", " + number);
-            for (int i = 1; i <= n; i++) {
-                if (previousNumber + number == i) {
-                    System.out.print(", " + i);
-                    previousNumber = number;
-                    number = i;
-                }
-            }
-            System.out.println();
         } else {
-            System.out.println("Dãy số cần tìm là: 0");
+            if (n == 1) {
+                System.out.println("Dãy số cần tìm là: " + previousNumber);
+            } else if (n == 2) {
+                System.out.println("Dãy số cần tìm là: 0, 1");
+            } else {
+                System.out.println("Dãy số cần tìm là: ");
+                System.out.print(previousNumber + ", " + number);
+                for (int i = 1; i <= n - 2; i++) {
+                    nextNumber = previousNumber + number;
+                    previousNumber = number;
+                    number = nextNumber;
+                    System.out.print(", " + nextNumber);
+                }
+                System.out.println();
+            }
         }
         sc.close();
     }
